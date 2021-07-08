eac87b92c2be102cb73abb1bcbf40a5409638a2b
agusl <agustin.lopezlacuara@ing.austral.edu.ar>
2021-07-08 00:35:00 -0300

    updates script

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(jibbyjabber.hopto.org)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="agustin.lopezlacuara@ing.austral.edu.ar"
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
